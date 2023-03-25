0da6280824545913675b1cd947772d98c64517e7
braxtonshaffie <braxtonshaffie@gmail.com>
2023-03-25 13:37:02 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove("main");
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML=" Braxton is the champion";
+
